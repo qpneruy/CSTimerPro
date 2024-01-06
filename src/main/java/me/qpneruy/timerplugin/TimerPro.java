8405aa10364911bbaa720a47f99daf7c374839bc
huuti <huutinh0916@gmail.com>
2024-01-06 17:09:47 +0700

    first commit

--- src/main/java/me/qpneruy/timerplugin/TimerPro.java
+++ src/main/java/me/qpneruy/timerplugin/TimerPro.java
@@ -1,15 +1,13 @@
 package me.qpneruy.timerplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import org.bukkit.scheduler.BukkitScheduler;
+
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
+
 
 public final class TimerPro extends JavaPlugin {
-    public current_time
     @Override
     public void onEnable() {
         saveDefaultConfig();
@@ -17,20 +15,20 @@ public final class TimerPro extends JavaPlugin {
         System.out.println("||     Plugin is started      ||");
         System.out.println("-------------------------------");
         getServer().getPluginManager().registerEvents(new JoinListener(this), this);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ss");
-        LocalTime localTime = LocalTime.now();
-        String currentTime = dtf.format(localTime);
-        int secondsUntilNextMinute = 60 - Integer.parseInt(currentTime);
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ss");
+//        LocalTime localTime = LocalTime.now();
+//        String currentTime = dtf.format(localTime);
+//        int secondsUntilNextMinute = 60 - Integer.parseInt(currentTime);
 
-        BukkitScheduler scheduler = getServer().getScheduler();
-        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
-            @Override
-            public void run() {
-                // Bắt đầu vòng lặp sự kiện sau khi đợi đến phút mới
-                BukkitRunnable task = new YourTask(YourPlugin.this);
-                task.runTaskTimer(YourPlugin.this, 0L, 100L);
-            }
-        }, secondsUntilNextMinute * 20L); // Chuyển đổi giây thành tick (20 tick = 1 giây);
+//        BukkitScheduler scheduler = getServer().getScheduler();
+//        scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+//            @Override
+//            public void run() {
+//                // Bắt đầu vòng lặp sự kiện sau khi đợi đến phút mới
+//                BukkitRunnable task = new YourTask(YourPlugin.this);
+//                task.runTaskTimer(YourPlugin.this, 0L, 100L);
+//            }
+//        }, secondsUntilNextMinute * 20L);
     }
 
     @Override
